83d94a1eb0f98ac865dd3617aaa719c5b9b0f60d
owfdr <owfdr@ourworld.center>
2023-06-25 21:48:50 +0900

    feat: live-stream

---
+++ src/live-stream.js
@@ -0,0 +1,55 @@
+import openai from "./openai.js";
+import {
+  always,
+  compose,
+  defaultTo,
+  forEach,
+  invoker,
+  pathOr,
+  reject,
+  replace,
+  split,
+  tryCatch,
+} from "ramda";
+
+const question = process.argv[2];
+
+const toString = invoker(0, "toString");
+
+const toArray = split("\n");
+
+const filterArray = reject((x) => x === "");
+
+const formatString = replace(/^data: /, "");
+
+const parseString = tryCatch(JSON.parse, always({}));
+
+const extractContent = pathOr("", ["choices", 0, "delta", "content"]);
+
+const writeToStdout = (x) => process.stdout.write(x);
+
+const takeResponse = compose(filterArray, toArray, toString);
+
+const messageReady = compose(
+  defaultTo(""),
+  extractContent,
+  parseString,
+  formatString
+);
+
+const typewriteEffect = compose(writeToStdout, messageReady);
+
+const animateIt = compose(forEach(typewriteEffect), takeResponse);
+
+const res = await openai.createChatCompletion(
+  {
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: question }],
+    stream: true,
+  },
+  {
+    responseType: "stream",
+  }
+);
+
+res.data.on("data", animateIt);
